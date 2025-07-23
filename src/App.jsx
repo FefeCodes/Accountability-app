dd475f650bc37205a91d2e0d6a73e0b2cf9756b6
FefeCodes <feranmiaina17@gmail.com>
2025-07-23 16:51:13 +0100

    Normalize line endings with .gitattributes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Try from './Try.jsx';
+
+function App() {
+  return(
+    <div>
+      <Try />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Try.jsx
@@ -0,0 +1,7 @@
+export default function Try(){
+    return(
+        <div>
+            <h1 className="text-4xl font-extrabold text-blue-600">Try Component</h1>
+        </div>
+    )
+}
